80c38faa39b021fac4c217ef51dbc0bcebf0208e
Drew Gray <drewgray@gmail.com>
2017-02-27 12:10:17 -0800

    create auth service to register users to app

--- angular-src/src/app/services/validate.service.ts
+++ angular-src/src/app/services/validate.service.ts
@@ -13,9 +13,9 @@ export class ValidateService {
     }
   }
 
-  validateEmail(user){
+  validateEmail(email){
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(user.email);
+    return re.test(email);
   }
 
 }
